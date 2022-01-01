de3c50cd9746ddb63ef1245fc755ceeb8e53b977
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-01-01 14:18:20 -0300

    -- update Code

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-function button_click(){
-    var image= document.getElementById("image-background")
-    var res = document.getElementById("resultado")
-    if(image.style.display ==='none' ){
-        console.log('ok')
-        res.innerHTML="<img src='img/lampada-acesa.png' >"  
-      }else {
-          console.log('[ERRO]')
-      } 
-}
+function button_clickOn() {
+
+    var image = document.getElementById("image-background");
+    var res = document.getElementById("resultado");
+
+    if (image.style.display === 'none') {
+        res.innerHTML = "<img src='img/lampada-acesa.png' >"
+    } else {
+        console.log('[ERRO]')
+    };
+};
+
+function button_clickOf() {
+
+    var image = document.getElementById("image-background");
+    var res = document.getElementById("resultado");
+
+    if (image.style.display === 'none') {
+        res.innerHTML = "<img src='img/lampada-apagada.png' >"
+    } else {
+        console.log('[ERRO]')
+    };
+};
